14da7e7bd71dbe171535b310572b812c6fcf730e
Joao Goncalves <Joao Goncalves@Joao-Laptop>
2017-11-09 21:46:24 +0000

    Criado ficheiro para ler rules.cf

--- src/antiSpamFilter/RuleScanner.java
+++ src/antiSpamFilter/RuleScanner.java
@@ -1,5 +1,30 @@
 package antiSpamFilter;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class RuleScanner {
+	
+	public void readFile(String filelocation) {
+		
+	try {
+
+        Scanner input = new Scanner(filelocation);
+
+        File file = new File(input.nextLine());
+
+        input = new Scanner(file);
+
+        while (input.hasNextLine()) {
+            String line = input.nextLine();
+            System.out.println(line);
+        }
+        input.close();
+
+    } catch (Exception ex) {
+        ex.printStackTrace();
+    }
+}
+
 
 }
