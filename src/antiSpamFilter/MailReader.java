502b911f1575cc7d2a2fe96fe06995d8e992b024
Júlio Bandeiras <Jlio Bandeiras@172.17.7.128>
2017-11-23 23:29:05 +0000

    Já lê mail

---
+++ src/antiSpamFilter/MailReader.java
@@ -0,0 +1,35 @@
+package antiSpamFilter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MailReader {
+	
+	public static void readHam(){
+	HashMap<String, ArrayList<String> > hash1 = new HashMap<String, ArrayList<String>>();
+	ArrayList<String> Array = new ArrayList<String>();
+	String line = "";
+	BufferedReader in;
+	try {
+		in = new BufferedReader(new FileReader("ham.log"));
+			while ((line = in.readLine()) != null) {
+			    String rules4u[] = line.split("\t");			   
+				for(int i=1;i<rules4u.length;i++){
+					Array.add(rules4u[i]);
+				}
+				 hash1.put(rules4u[0],Array);
+			}
+			in.close();
+		} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+		}
+	System.out.println(hash1);
+	}
+	
+	public static void main(String[] pussy){
+		MailReader.readHam();
+	}
+}
